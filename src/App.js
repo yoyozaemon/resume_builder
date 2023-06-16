9cbfae0bd30c95bef523c6409bb06eed830fb780
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-06-16 09:25:46 +0530

    complete code added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+import PdfComponent from './components/PdfComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="bg-white p-0">
+
+      <Navigation></Navigation>
+
+      <Routes>
+        <Route path="/" element={ <Resume/> } exact></Route>
+        <Route path="/preview" element={<PdfComponent/>}></Route>
+      </Routes>
+      
+      <Footer></Footer>
+
+    </Container>
   );
 }
 
